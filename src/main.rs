745a501c0dad897ea0147c0a49210266d71ad789
Caleb McCarthy <caleb.andrew.mccarthy@ibm.com>
2019-06-16 15:04:31 -0500

    if n queens is found mark them in n by n matrix and print it out

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 fn main() {
-    println!("");
+    println!("finding n queens");
     let mut vec:Vec<Vec<i32>> = Vec::new();
-    for _i in 0..4 {vec.push(vec![0,0,0,0])};
+    let sizeofvec = 4;
+    for _i in 0..sizeofvec {
+        let mut minivec = Vec::new();
+        for j in 0..sizeofvec {
+            minivec.push(0);
+        }
+        vec.push(minivec);
+    };
     
     
     nqueens(&mut vec,0);
-    attacked(&vec,2,3);
+ 
    
+    for i in 0..sizeofvec {
+        println!("{:?}",vec[i]);
+    }
 
-    println!("{:?}",vec);
 
 }
 /*
-    ToDo Check if peice is attacked
+     Checks if peice is attacked
 */
 fn attacked(input:&Vec<Vec<i32>>,row:i32,col:i32)-> bool {
    
@@ -21,10 +30,10 @@ fn attacked(input:&Vec<Vec<i32>>,row:i32,col:i32)-> bool {
 
         for j in 0..input.len() {
             if input[i][j] == 1 {
-                if i == col as usize || j == row as usize {
+                if i == row as usize || j == col as usize {
                     return true
                 }
-                if (row - col) == (j as i32) - (i as i32) || (row + col) == ((j as i32)+(i as i32)) {
+                if (row - col) == (i as i32) - (j as i32) || (row + col) == ((j as i32)+(i as i32)) {
                     return true
                 }
             }
@@ -35,13 +44,41 @@ fn attacked(input:&Vec<Vec<i32>>,row:i32,col:i32)-> bool {
     return false;
 }
 
-fn nqueens( input:&mut Vec<Vec<i32>>,col:i32)-> bool{
-    if col == input.len() as i32 {
-        return true;
+
+fn countqueens(input:&Vec<Vec<i32>>)-> i32 {
+    let mut count = 0;
+    for i in 0..input.len() {
+        for j in 0..input.len() {
+            if input[i][j] == 1 {
+                count = count +1;
+            }
+        }
     }
-    input[0][col as usize] = 8;
-     return nqueens(input,col+1);
+    count
+}
 
 
-   
+fn nqueens( input:&mut Vec<Vec<i32>>,row:i32)-> bool{
+    if row == (input.len() + 1) as i32 {
+        return false;
+    }
+    for i in 0..input.len() {
+        let attackedme = attacked(&input,row,i as i32);
+        
+        if !attackedme{
+            input[row as usize][i as usize] = 1;
+            if !nqueens(input,row+1){
+                
+                
+                input[row as usize][i as usize] = 0;
+            }
+        }
+    }
+     
+
+    if countqueens(input) == input.len() as i32 {
+            return true;
+    }
+    false
+
 }
